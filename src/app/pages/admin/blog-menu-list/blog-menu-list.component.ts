de5d3aa108a15a7e8f2d8cfe3d6f43872c406e66
ismet kizgin <info@ismetkizgin.com>
2020-10-27 22:55:09 +0300

    feat: added blog menu delete window

--- src/app/pages/admin/blog-menu-list/blog-menu-list.component.ts
+++ src/app/pages/admin/blog-menu-list/blog-menu-list.component.ts
@@ -3,6 +3,8 @@ import { BlogMenu } from './blog-menu-list.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog } from '@angular/material/dialog';
 import { BlogMenuService } from '../../../utils/services';
+import { DialogWindowComponent } from '../../../components';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-blog-menu-list',
@@ -13,6 +15,7 @@ export class BlogMenuListComponent implements OnInit {
   constructor(
     private _snackBar: MatSnackBar,
     private _dialog: MatDialog,
+    private _translateService: TranslateService,
     private _blogMenuService: BlogMenuService
   ) {}
 
@@ -30,5 +33,41 @@ export class BlogMenuListComponent implements OnInit {
     } catch (error) {
       this._blogMenuService.errorNotification(error);
     }
+  }
+
+  async componentDelete(BlogMenuID) {
+    const diologRef = this._dialog.open(DialogWindowComponent, {
+      data: {
+        message: 'Are you sure you want to delete the blog menu ?',
+        icon: 'fa fa-exclamation',
+      },
+    });
+
+    diologRef.afterClosed().subscribe(async (result: boolean) => {
+      if (result) {
+        try {
+          await this._blogMenuService.deleteAsync({ BlogMenuID });
+          this.blogMenus.splice(
+            this.blogMenus.findIndex(
+              (blogMenu) => blogMenu.BlogMenuID == BlogMenuID
+            ),
+            1
+          );
+          let notificationMessage: string;
+          this._translateService
+            .get('Blog menu information was successfully deleted')
+            .subscribe((value) => (notificationMessage = value));
+
+          this._snackBar.open(notificationMessage, 'X', {
+            duration: 3000,
+            panelClass: 'notification__success',
+            verticalPosition: 'bottom',
+            horizontalPosition: 'right',
+          });
+        } catch (error) {
+          this._blogMenuService.errorNotification(error);
+        }
+      }
+    });
   }
 }
