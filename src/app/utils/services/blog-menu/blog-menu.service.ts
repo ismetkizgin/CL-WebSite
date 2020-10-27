ff76f2a17ac0239fe0927096c1cf46aa30c03031
ismet kizgin <info@ismetkizgin.com>
2020-10-27 14:06:35 +0300

    feat: added blog menu service

---
+++ src/app/utils/services/blog-menu/blog-menu.service.ts
@@ -0,0 +1,99 @@
+import { Injectable } from '@angular/core';
+import { ApiFetchService } from '../api-fetch/api-fetch.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BlogMenuService {
+  constructor(
+    private _apiFetchService: ApiFetchService,
+    private _snackBar: MatSnackBar,
+    private _translateService: TranslateService
+  ) {}
+
+  async listAsync() {
+    return await this._apiFetchService.requestAsync(
+      'GET',
+      'blog-menu',
+      null,
+      true
+    );
+  }
+
+  async deleteAsync(values) {
+    return await this._apiFetchService.requestAsync(
+      'DELETE',
+      'blog-menu',
+      values,
+      true
+    );
+  }
+
+  async findAsync(userID) {
+    return await this._apiFetchService.requestAsync(
+      'GET',
+      `blog-menu/${userID}`,
+      null,
+      true
+    );
+  }
+
+  async insertAsync(values) {
+    return await this._apiFetchService.requestAsync(
+      'POST',
+      'blog-menu',
+      values,
+      true
+    );
+  }
+
+  async updateAsync(values) {
+    return await this._apiFetchService.requestAsync(
+      'PUT',
+      'blog-menu',
+      values,
+      true
+    );
+  }
+
+  errorNotification(error) {
+    let errorMessage: string;
+    switch (error.status) {
+      case 401:
+        this._translateService
+          .get('Unauthorized transaction !')
+          .subscribe((value) => (errorMessage = value));
+        break;
+      case 409:
+        this._translateService
+          .get('Such an blog menu is already registered in the system !')
+          .subscribe((value) => (errorMessage = value));
+        break;
+      case 417:
+        this._translateService
+          .get('Please enter correct blog menu information !')
+          .subscribe((value) => (errorMessage = value));
+        break;
+      case 404:
+        this._translateService
+          .get('No blog menu record found in the system !')
+          .subscribe((value) => (errorMessage = value));
+        break;
+      default:
+        this._translateService
+          .get(
+            'Server error occurred, please try again later If the error persists, we ask you to report this to the authorities'
+          )
+          .subscribe((value) => (errorMessage = value));
+        break;
+    }
+    this._snackBar.open(errorMessage, 'X', {
+      duration: 4000,
+      panelClass: 'notification__error',
+      verticalPosition: 'bottom',
+      horizontalPosition: 'right',
+    });
+  }
+}
--- src/app/utils/services/index.ts
+++ src/app/utils/services/index.ts
@@ -2,3 +2,4 @@ export * from './api-fetch/api-fetch.service';
 export * from './auth/auth.service';
 export * from './user/user.service';
 export * from './component-menu/component-menu.service';
+export * from './blog-menu/blog-menu.service';
