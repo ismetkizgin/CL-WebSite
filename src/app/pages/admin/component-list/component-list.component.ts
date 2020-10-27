a50e667e0b3812d43b64bcd4335de12e3f580853
ismet kizgin <info@ismetkizgin.com>
2020-10-27 22:14:18 +0300

    feat: added component delete window

--- src/app/pages/admin/component-list/component-list.component.ts
+++ src/app/pages/admin/component-list/component-list.component.ts
@@ -3,6 +3,8 @@ import { ComponentModel } from './component-list.model';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogWindowComponent } from '../../../components';
 import { ComponentService } from '../../../utils/services';
+import { TranslateService } from '@ngx-translate/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-component-list',
@@ -12,7 +14,9 @@ import { ComponentService } from '../../../utils/services';
 export class ComponentListComponent implements OnInit {
   constructor(
     private _dialog: MatDialog,
-    private _componentService: ComponentService
+    private _componentService: ComponentService,
+    private _snackBar: MatSnackBar,
+    private _translateService: TranslateService
   ) {}
 
   components: Array<ComponentModel>;
@@ -39,6 +43,33 @@ export class ComponentListComponent implements OnInit {
         message: 'Are you sure you want to delete the component ?',
         icon: 'fa fa-exclamation',
       },
+    });
+
+    diologRef.afterClosed().subscribe(async (result: boolean) => {
+      if (result) {
+        try {
+          await this._componentService.deleteAsync({ ComponentID });
+          this.components.splice(
+            this.components.findIndex(
+              (component) => component.ComponentID == ComponentID
+            ),
+            1
+          );
+          let notificationMessage: string;
+          this._translateService
+            .get('Component information was successfully deleted')
+            .subscribe((value) => (notificationMessage = value));
+
+          this._snackBar.open(notificationMessage, 'X', {
+            duration: 3000,
+            panelClass: 'notification__success',
+            verticalPosition: 'bottom',
+            horizontalPosition: 'right',
+          });
+        } catch (error) {
+          this._componentService.errorNotification(error);
+        }
+      }
     });
   }
 }
