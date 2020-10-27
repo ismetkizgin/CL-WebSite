45216784d83ac5ea70a46f970f0588ff37116f17
frknsgt <furkan.v.sk@gmail.com>
2020-10-27 14:57:34 +0300

    feat: add and edit blog menu window added

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -4,3 +4,4 @@ export * from './pagination/pagination.component';
 export * from './add-component/add-component.component';
 export * from './password-change/password-change.component';
 export * from './pagination/pagination.component';
+export * from './add-blog-menu/add-blog-menu.component';
