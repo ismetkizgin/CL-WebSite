8954a3d497b446b757ec6997b5de8945063b2254
ismet kizgin <info@ismetkizgin.com>
2020-10-27 21:56:06 +0300

    feat: added component service

--- src/app/utils/services/index.ts
+++ src/app/utils/services/index.ts
@@ -3,3 +3,4 @@ export * from './auth/auth.service';
 export * from './user/user.service';
 export * from './component-menu/component-menu.service';
 export * from './blog-menu/blog-menu.service';
+export * from './component/component.service';
