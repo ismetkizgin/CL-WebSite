613dbf0a2b75a5bc486a3d977012ebb5af6eb217
ismet kizgin <info@ismetkizgin.com>
2021-01-14 19:37:30 +0300

    fix: fix for blog page

--- src/app/pages/client/blogs/blogs.component.ts
+++ src/app/pages/client/blogs/blogs.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Blog } from 'src/app/models';
 import { BlogService } from '../../../utils/services/blog/blog.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
-  styleUrls: ['./blogs.component.scss']
+  styleUrls: ['./blogs.component.scss'],
 })
 export class BlogsComponent implements OnInit {
-
   constructor(
     private _blogService: BlogService,
-    private _activateRoute: ActivatedRoute
-    ) { }
+    private _activateRoute: ActivatedRoute,
+    private _router: Router
+  ) {
+    this._router.events.subscribe((evt) => {
+      if (evt instanceof NavigationEnd) {
+        this.ngOnInit();
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+
   searchText: String;
-  blogsOriginal: Array<Blog>;
   blogs: Array<Blog>;
   async ngOnInit() {
-    const BlogMenuID=this._activateRoute.snapshot.paramMap.get('BlogMenuID');
+    const BlogMenuID = this._activateRoute.snapshot.paramMap.get('BlogMenuID');
     try {
-      this.blogsOriginal = <Array<Blog>>(await this._blogService.listAsync());
-      this.blogs = this.blogsOriginal.filter((data) => data.BlogMenuID == BlogMenuID);
+      this.blogs = <Array<Blog>>await this._blogService.listAsync({
+        BlogMenuID,
+        BlogState: true,
+      });
     } catch (error) {
       this._blogService.errorNotification(error);
     }
   }
-
 }
